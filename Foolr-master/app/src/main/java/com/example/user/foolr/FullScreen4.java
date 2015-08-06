c2056fc113934d835e49d08b774140a77140abd8
Nebex <struhlboy@gmail.com>
2015-08-06 10:13:04 -0400

    Revert "Revert "Done""
    This reverts commit 331f9bc11afb0bfb6b8bf15edd730e1149319ca4.

---
+++ Foolr-master/app/src/main/java/com/example/user/foolr/FullScreen4.java
@@ -0,0 +1,28 @@
+package com.example.user.foolr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FullScreen4 extends Vault{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.full_screen_4);
+
+        Intent x = new Intent();
+        x = getIntent();
+
+        Button back = (Button)this.findViewById(R.id.back);
+        back.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Vault.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
